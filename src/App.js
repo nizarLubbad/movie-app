264c8d6a8447054cc86485848b440bf7694162c8
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-26 13:11:12 +0300

    Add NavBar component and styles; update App component to include NavBar

--- src/App.js
+++ src/App.js
@@ -48,5 +48,31 @@ const tempWatchedData = [
 ];
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <NavBar />
+    </>
+  );
+}
+
+function NavBar() {
+  const [query, setQuery] = useState("");
+
+  <nav className="nav-bar">
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  </nav>;
 }
