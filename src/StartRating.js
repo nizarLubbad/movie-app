0ad5d49f11cca75afd39091395641c86b3430202
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-27 15:41:51 +0300

    Refactor StartRating component: enhance rating interaction with temporary state for hover feedback

--- src/StartRating.js
+++ src/StartRating.js
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StartRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   return (
     <div style={containerStyle}>
@@ -25,11 +26,13 @@ export default function StartRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRating={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -37,13 +40,18 @@ export default function StartRating({ maxRating = 5 }) {
 const starStyle = {
   display: "block",
   width: "24px",
-  hight: "24px",
+  height: "24px",
   cursor: "pointer",
 };
 
-function Star({ onRating, full }) {
+function Star({ onRating, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={starStyle} onClick={onRating}>
+    <span
+      style={starStyle}
+      onClick={onRating}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +71,7 @@ function Star({ onRating, full }) {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
+            strokeWidth={2}
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
